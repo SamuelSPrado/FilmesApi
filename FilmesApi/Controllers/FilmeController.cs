e00f034847f07d634f5dc95e3cda319cb94a1ed1
Samuel <samuelsantanaprado@gmail.com>
2023-06-24 22:19:23 -0300

    Start of building the API

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,27 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        if (!string.IsNullOrEmpty(filme.Titulo))
+        {
+            filmes.Add(filme);
+            Console.WriteLine("Título: " + filme.Titulo);
+            Console.WriteLine("Gênero: " + filme.Genero);
+            Console.WriteLine("Diretor : " + filme.Diretor);
+            Console.WriteLine("Ano de lançamento: " + filme.Ano);
+            Console.WriteLine("Duração: " + filme.Duracao + " minutos");
+        }
+        
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "O título deve conter de 3 à 50 caracteres!")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres!")]
+    public string Genero { get; set; }
+
+    [Required, StringLength(70, MinimumLength = 2, ErrorMessage = "É necessário incluir o nome do Diretor")]
+    public string Diretor { get; set; }
+
+    [Required, Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos!")]
+    public int Duracao { get; set; }
+
+    [Required(ErrorMessage ="Necessário informar o ano de lançamento")]
+    public int Ano { get; set; }
+
+}
